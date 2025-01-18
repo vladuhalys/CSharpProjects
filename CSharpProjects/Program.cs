0e7fb9952131c15f4b775714a3404a0ac6ba2b22
Vladyslav Lysenko <vlad.lysenko1999@gmail.com>
2025-01-18 14:25:40 +0200

    Init Commit

---
+++ CSharpProjects/Program.cs
@@ -0,0 +1,3 @@
+﻿using CreationalPatterns.FactoryMethod;
+
+ClientCode.Main();
---
+++ CreationalPatterns/FactoryMethod/ClientCode.cs
@@ -0,0 +1,9 @@
+﻿namespace CreationalPatterns.FactoryMethod;
+
+public class ClientCode
+{
+    public static void Main()
+    {
+        Console.WriteLine("Call CreationalPatterns.FactoryMethod");
+    }
+}
